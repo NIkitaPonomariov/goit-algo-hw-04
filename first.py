115bda1cead0f134b63d47a54dffd26fd125758b
Nikita <ponomariovnikita02@gmail.com>
2024-04-26 14:25:47 +0300

    первое второе и третье дз

--- first.py
+++ first.py
@@ -1,5 +1,3 @@
-#first 
-
 
 def total_salary(path):
     total_salary = 0
@@ -25,23 +23,3 @@ def total_salary(path):
     average_salary = total_salary / num_developers
     return total_salary, average_salary
 
-
-#second
-
-def get_cats_info(path):
-    cats_info = []
-    
-    try:
-        with open(path, 'r', encoding='utf-8') as file:
-            for line in file:
-                cat_id, name, age = line.strip().split(',')
-                cat_info = {"id": cat_id, "name": name, "age": age}
-                cats_info.append(cat_info)
-    except FileNotFoundError:
-        print("Файл не знайдено.")
-        return None
-    except Exception as e:
-        print(f"Помилка: {e}")
-        return None
-    
-    return cats_info
